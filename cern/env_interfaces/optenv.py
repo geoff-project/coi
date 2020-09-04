71cccb38dae0d9015bd79fcddb5d5707b41baba7
Nico Madysa <nico.madysa@cern.ch>
2020-09-04 16:46:48 +0200

    Reorganize optenv module.

--- cern/env_interfaces/optenv.py
+++ cern/env_interfaces/optenv.py
@@ -1,26 +1,86 @@
 #!/usr/bin/env python
-"""Provides `OptEnv`, which can be used for RL and numerical optimization. """
+"""An environment interface that supports RL and numerical optimization."""
+
+# pylint: disable = abstract-method, too-few-public-methods
 
 import gym
 
+__all__ = [
+    'OptEnv',
+    'OptGoalEnv',
+    'OptimizeMixin',
+]
 
-class OptEnv(gym.Env):
-    """Interface for optimizable environments.
 
-    An optimizable environment is mostly identical to regular Gym environment,
-    but it additionally provides a `step_opt()` method. To describe the bounds
-    of valid inputs to it, it also has an attribute `opt_action_space`.
+class OptimizeMixin:
+    """Additional mix-in for environments that are optimizable.
+
+    Fundamentally, an environment contains a hidden state on which actions can
+    performed. Each action causes a transition from one state to another. Each
+    transition produces an observation and a reward.
+
+    An environment is *optimizable* if additionally, there are certain
+    *parameters* that can be set to transition to to a certain state. And each
+    transition is associated with a *loss* that shall be minimized.
+
+    The difference between actions and parameters is: actions describe a step
+    that shall be taken in the phase space of states; parameters describe the
+    point in phase space that shall be moved to. A parameter may be e.g. the
+    electric current supplied to a magnet, and an action may be the value by
+    which to increase or decrease that current.
+
+    The difference between the parameters and the hidden state is that the
+    parameters may describe only a *subset* of the state. There may be state
+    variables that cannot be influenced by the optimizer.
+
+    This mix-in does not provide any logic of its own. It merely defines a
+    uniform interface through which a client may connect a numerical optimizer
+    to a class that is normally used in reinforcement learning.
+
+    Typically, you don't want to inherit from this mix-in directly. This
+    package provides several classes that extend the `gym.Env` interface with
+    it.
+
+    Attributes:
+        optimization_space: A `gym.Space` instance that describes the phase
+            space of parameters. This may be the same or different from the
+            `gym.Env.action_space`.
     """
-    opt_action_space = None
+    optimization_space = None
 
-    def step_opt(self, opt_action):
-        """Perform an optimizer step.
+    def compute_loss(self, parameters) -> float:
+        """Perform an optimization step.
 
-        This function is similar to `step()`, but is tuned for numerical
-        optimizers. It should not perform a single step through the phase
-        space, but instead immediately go to the given point.
+        This function is similar to `step()`, but it accepts parameters instead
+        of an action. See the class docstring for the difference.
 
-        In addition, this function only returns the reward. Other information
-        is not necessary.
+        This function may modify the environment, but it should fundamentally
+        be stateless: Calling `compute_loss()` twice with the same parameters
+        should return the same loss, excepting any stochastic noise.
+
+        Args:
+            parameters: The parameters for which the loss shall be calculated.
+                This should be within `self.optimization_space`, but it must at
+                least have the same structure.
+
+        Returns:
+            The loss associated with these parameters. Numerical optimizers may
+            want to minimize that loss.
         """
         raise NotImplementedError()
+
+
+class OptEnv(gym.Env, OptimizeMixin):
+    """An optimizable environment.
+
+    This is an intersection of `Env` and `OptimizeMixin`. See the respective
+    abstract classes for their documentation.
+    """
+
+
+class OptGoalEnv(gym.GoalEnv, OptimizeMixin):
+    """An optimizable multi-goal environment.
+
+    This is an intersection of `GoalEnv` and `OptimizeMixin`. See the
+    respective abstract classes for their documentation.
+    """
