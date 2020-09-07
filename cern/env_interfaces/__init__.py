e94e0733c92e1b8ede78e5b14e8b23eab479181c
Nico Madysa <nico.madysa@cern.ch>
2020-09-07 10:10:28 +0200

    Re-export separable environments.

--- cern/env_interfaces/__init__.py
+++ cern/env_interfaces/__init__.py
@@ -2,5 +2,11 @@
 """Interfaces for common optimization between DESY and CERN accelerators."""
 
 from .optenv import OptEnv, OptGoalEnv, OptimizeMixin
+from .sepenv import (
+    SeparableEnv,
+    SeparableGoalEnv,
+    SeparableOptEnv,
+    SeparableOptGoalEnv,
+)
 
 __version__ = "0.0.1"
