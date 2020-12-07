1a50246df71e2ee30b94ad89ada9521527a53dcc
Nico Madysa <nico.madysa@cern.ch>
2020-12-07 09:56:03 +0100

    Extend Pylint and MyPy checks.

--- tests/test_env_checker.py
+++ tests/test_env_checker.py
@@ -3,6 +3,8 @@
 
 # pylint: disable = missing-class-docstring, missing-function-docstring
 
+import typing as t
+
 import gym
 import numpy as np
 from matplotlib import pyplot
@@ -25,15 +27,15 @@ class MultiGoalParabola(SeparableOptGoalEnv):
         "cern.machine": Machine.NoMachine,
     }
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.pos = self.action_space.sample()
         self.goal = self.action_space.sample()
 
     @property
-    def distance(self):
+    def distance(self) -> float:
         return np.linalg.norm(self.pos - self.goal)
 
-    def reset(self):
+    def reset(self) -> np.ndarray:
         self.pos = self.action_space.sample()
         self.goal = self.action_space.sample()
         return dict(
@@ -42,7 +44,9 @@ class MultiGoalParabola(SeparableOptGoalEnv):
             desired_goal=self.goal.copy(),
         )
 
-    def compute_observation(self, action, info):
+    def compute_observation(
+        self, action: np.ndarray, info: t.Dict
+    ) -> t.Dict[str, np.ndarray]:
         self.pos += action
         return dict(
             observation=np.array([self.distance]),
@@ -50,38 +54,48 @@ class MultiGoalParabola(SeparableOptGoalEnv):
             desired_goal=self.goal.copy(),
         )
 
-    def compute_reward(self, achieved_goal, desired_goal, info):
+    def compute_reward(
+        self,
+        achieved_goal: np.ndarray,
+        desired_goal: np.ndarray,
+        info: t.Dict[str, t.Any],
+    ) -> float:
         return max(-self.distance, self.reward_range[0])
 
-    def compute_done(self, obs, reward, info):
+    def compute_done(
+        self,
+        obs: t.Dict[str, np.ndarray],
+        reward: float,
+        info: t.Dict[str, t.Any],
+    ) -> bool:
         success = bool(obs["observation"] < 0.05)
         done = success or obs not in self.observation_space
         if done:
             info["success"] = success
         return done
 
-    def get_initial_params(self):
+    def get_initial_params(self) -> np.ndarray:
         return self.reset()["achieved_goal"]
 
-    def compute_single_objective(self, params):
+    def compute_single_objective(self, params: np.ndarray) -> float:
         self.pos = params.copy()
         return self.distance
 
-    def render(self, mode="human"):
+    def render(self, mode: str = "human", **kwargs: t.Any) -> t.Any:
         if mode in ("human", "qtembed"):
             xdata, ydata = zip(self.pos, self.goal)
             pyplot.scatter(xdata, ydata, c=[0, 1])
             return None
         if mode == "ansi":
             return f"{self.pos} -> {self.goal}"
-        return super().render(mode)
+        return super().render(mode, **kwargs)
 
-    def seed(self, seed=None):
+    def seed(self, seed: t.Optional[int] = None) -> t.List[int]:
         return [
             *self.action_space.seed(seed),
             *self.observation_space.seed(seed),
         ]
 
 
-def test_check_env():
+def test_check_env() -> None:
     check_env(MultiGoalParabola())
