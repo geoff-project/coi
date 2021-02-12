1156739c8d06160ac6f957be27cd425bb9ba2f83
Nico Madysa <nico.madysa@cern.ch>
2021-02-12 15:33:34 +0100

    Add test to ensure protocol-like behavior of Problem.

---
+++ tests/test_problem.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""Test that `Problem` does not require direct inheritance."""
+# pylint: disable = missing-class-docstring, missing-function-docstring
+
+import typing as t
+
+from cernml import coi
+
+
+def test_problem_is_abstract() -> None:
+    class NonInheritingProblem:
+        metadata: t.Dict[str, t.Any] = {"render.modes": []}
+
+        def close(self) -> None:
+            pass
+
+        def render(self, mode: str = "human") -> t.Any:
+            raise NotImplementedError()
+
+        def unwrapped(self) -> "NonInheritingProblem":
+            return self
+
+    assert issubclass(NonInheritingProblem, coi.Problem)
+
+
+def test_problem_requires_close() -> None:
+    class NullProblemo:
+        metadata: t.Dict[str, t.Any] = {"render.modes": []}
+
+        def render(self, mode: str = "human") -> t.Any:
+            raise NotImplementedError()
+
+        def unwrapped(self) -> "NullProblemo":
+            return self
+
+    assert not issubclass(NullProblemo, coi.Problem)
