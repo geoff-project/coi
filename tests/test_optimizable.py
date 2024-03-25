750cff72484630f235461a2c25ea1ffd00721bab
Nico Madysa <nico.madysa@cern.ch>
2024-03-25 15:35:24 +0100

    Add tests for BaseSingleOptimizable and BaseFunctionOptimizable.

---
+++ tests/test_optimizable.py
@@ -0,0 +1,76 @@
+# SPDX-FileCopyrightText: 2020-2024 CERN
+# SPDX-FileCopyrightText: 2023-2024 GSI Helmholtzzentrum für Schwerionenforschung
+# SPDX-FileNotice: All rights not expressly granted are reserved.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later OR EUPL-1.2+
+
+"""Test SingleOptimizable, FunctionOptimizable and their ABCs."""
+
+import typing as t
+
+import numpy as np
+import pytest
+from gymnasium.spaces import Box
+
+from cernml import coi
+
+
+@pytest.mark.parametrize("cls", [coi.SingleOptimizable, coi.BaseSingleOptimizable])
+def test_single_optimizable_defaults(cls: type[coi.SingleOptimizable]) -> None:
+    assert cls.metadata["render_modes"] == []
+    assert cls.render_mode is None
+    assert getattr(cls, "optimization_space", None) is None
+    assert cls.objective_range == (-float("inf"), float("inf"))
+    assert len(cls.constraints) == 0
+    assert cls.objective_name == ""
+    assert len(cls.param_names) == 0
+    assert len(cls.constraint_names) == 0
+
+
+@pytest.mark.parametrize("cls", [coi.FunctionOptimizable, coi.BaseFunctionOptimizable])
+def test_function_optimizable_defaults(cls: type[coi.FunctionOptimizable]) -> None:
+    class Subclass(cls):  # type: ignore[misc,valid-type]
+        def get_optimization_space(self, cycle_time: float) -> Box:
+            return Box(-1, 1, ())
+
+        def get_initial_params(self, cycle_time: float) -> np.ndarray:
+            return cycle_time + np.zeros(())
+
+        def compute_function_objective(
+            self, cycle_time: float, params: np.ndarray
+        ) -> float:
+            return np.sum(params) + cycle_time
+
+    problem = Subclass()
+    assert problem.metadata["render_modes"] == []
+    assert problem.objective_range == (-float("inf"), float("inf"))
+    assert len(problem.constraints) == 0
+    assert problem.get_objective_function_name() is None
+    assert problem.get_param_function_names() == []
+    assert problem.override_skeleton_points() is None
+
+
+@pytest.mark.parametrize(
+    "cls", [coi.BaseSingleOptimizable, coi.BaseFunctionOptimizable]
+)
+def test_base_optimizable_sets_render_mode(cls: type[coi.SingleOptimizable]) -> None:
+    class Subclass(cls):  # type: ignore[misc,valid-type]
+        metadata = {"render_modes": ["rgb_array"]}
+
+        def get_initial_params(
+            self, cycle_time: t.Union[float, None] = None
+        ) -> np.ndarray:
+            return np.zeros(2)
+
+        def get_optimization_space(self, cycle_time: float) -> Box:
+            return Box(-2, 2, shape=(2,))
+
+        def compute_single_objective(self, params: np.ndarray) -> float:
+            return 0.0
+
+        def compute_function_objective(
+            self, params: np.ndarray, cycle_time: float
+        ) -> float:
+            return 0.0
+
+    assert Subclass(render_mode="rgb_array").render_mode == "rgb_array"
