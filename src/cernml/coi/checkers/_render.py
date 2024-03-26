82b0e8436835fe774ff96cefcad25bbea8500011
Nico Madysa <nico.madysa@cern.ch>
2024-03-26 17:59:07 +0100

    Add checks for render modes rgb_array_list and ansi_list.

--- src/cernml/coi/checkers/_render.py
+++ src/cernml/coi/checkers/_render.py
@@ -21,19 +21,46 @@ else:
     MPL_AVAILABLE = True
 
 
-def get_render_mode_checks() -> t.Dict[str, t.Callable[..., None]]:
+def get_render_mode_checks() -> dict[str, t.Callable[..., None]]:
     """Return a mapping from render mode to checker for that mode."""
-    # TODO: Add rgb_array_list and ansi_list; Handle new semantics of
-    # `human` (render() call not necessary)
+    # TODO: Handle new semantics of `human` (render() call not necessary)
     checks = {
+        "rgb_array_list": assert_rgb_array_list,
         "rgb_array": assert_rgb_array,
         "human": assert_human,
+        "ansi_list": assert_ansi_list,
         "ansi": assert_ansi,
     }
     # Don't fail on missing matplotlib.
     if MPL_AVAILABLE:
         checks["matplotlib_figures"] = assert_matplotlib_figures
     return checks
+
+
+def assert_rgb_array_list(result: t.Any) -> None:
+    """Assert that *result* is a list of RGB images.
+
+    Example:
+
+        >>> image = np.zeros((36, 36, 3))
+        >>> assert_rgb_array_list([image, image])
+        >>> assert_rgb_array_list([image, image, None, image])
+        Traceback (most recent call last):
+        ...
+        AssertionError: item #2 of the render('rgb_array_list')
+        result is invalid, see the exception above
+    """
+    assert is_iterable(
+        result
+    ), "render('rgb_array_list') should return a list of NumPy arrays, not {result!r}"
+    try:
+        for i, image in enumerate(result):  # noqa: B007
+            assert_rgb_array(image)
+    except AssertionError as exc:
+        raise AssertionError(
+            f"item #{i} of the render('rgb_array_list') result is "
+            f"invalid, see the exception above"
+        ) from exc
 
 
 def assert_rgb_array(result: t.Any) -> None:
@@ -65,6 +92,31 @@ def assert_human(result: t.Any) -> None:
     assert result is None, f"render('human') should return None, not {result!r}"
 
 
+def assert_ansi_list(result: t.Any) -> None:
+    """Assert that *result* is a list of strings or StringIO.
+
+    Example:
+
+        >>> assert_ansi_list(list("abcde"))
+        >>> assert_ansi_list(["1", "2", 3, "4"])
+        Traceback (most recent call last):
+        ...
+        AssertionError: image #2 of the render('rgb_array_list')
+        result is invalid, see the exception above
+    """
+    assert is_iterable(
+        result
+    ), "render('ansi_list') should return a list of strings, not {result!r}"
+    try:
+        for i, string in enumerate(result):  # noqa: B007
+            assert_ansi(string)
+    except AssertionError as exc:
+        raise AssertionError(
+            f"item #{i} of the render('ansi_list') result is "
+            f"invalid, see the exception above"
+        ) from exc
+
+
 def assert_ansi(result: t.Any) -> None:
     """Assert that *result* maches the "ansi" render mode.
 
@@ -84,6 +136,7 @@ def assert_matplotlib_figures(result: t.Any) -> None:
 
         >>> assert_matplotlib_figures(Figure())
         >>> assert_matplotlib_figures({"foo": Figure()})
+        >>> assert_matplotlib_figures([("foo", Figure())])
         >>> assert_matplotlib_figures("foo")
         Traceback (most recent call last):
         ...
@@ -122,7 +175,7 @@ def assert_mpl_figures_iterable(result: t.Iterable) -> None:
     """Assert that *result* is an iterable of figures and titles."""
     for item in t.cast(t.Iterable, result):
         title, figure = (
-            t.cast(t.Tuple[t.Any, t.Any], item) if is_iterable(item) else ("", item)
+            t.cast(tuple[t.Any, t.Any], item) if is_iterable(item) else ("", item)
         )
         _assert_mpl_figures_item(title, figure)
 
